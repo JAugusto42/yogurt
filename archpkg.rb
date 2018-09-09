ea79a1f3a7511d2e32aa00455caa5d95bf330747
José Augusto <joseaugusto.881@outlook.com>
2018-09-09 17:59:06 -0300

    Add more features

--- archpkg.rb
+++ archpkg.rb
@@ -7,11 +7,11 @@ require 'json'
 # funcionar depois refatorar
 # associar o package com o numero para q, em vez de passar o numero, passe o pacote correspondente.
 
-
 class Main
+  # [global vars]-------------------------------
+
   def initialize
-    puts 'arch pkg'
-    input = ARGV
+    raise 'Requires ruby >= 2.4.0' if RUBY_VERSION < '2.4.0'
 
     case ARGV[0]
     when '-S'
@@ -24,35 +24,46 @@ class Main
       help
 
     else
-      puts input.class
       puts 'Invalid Input! Try again...'
     end
   end
 
-  def install_pkg(packages)
+  def install_pkg
     # TODO
   end
 
   def search
+    pacman_local_dir = '/var/lib/pacman/local/' # to read all packges installed
+    packages_local = Dir.children(pacman_local_dir) # get all packages
+
     pkg = ARGV[1]
+
     puts ":: Seaching #{pkg} on aur..."
     url = "https://aur.archlinux.org/rpc/?v=5&type=search&arg=#{pkg}"
     buffer = open(url).read
     obj = JSON.parse(buffer)
     packages = obj['resultcount']
     packages_name = obj['results']
+
     names = packages_name.map { |result| result['Name'] }
     version = packages_name.map { |result| result['Version'] }
+    description = packages_name.map { |result| result['Description'] }
+
     puts ":: Found #{packages} packages"
-    count = 0
+    count = 1
+    exit unless packages > 0
     while count < packages
-      puts ":: aur/ #{count} #{names[count]} -> #{version[count]}"
+
+      name_and_version = "#{names[count]}-#{version[count]}"
+
+      check = print('Installed ') if packages_local.include?(name_and_version) # é o que???? uehueheuheuheueh
+
+      puts ":: \e[32m#{count}\e[0m aur/#{names[count]} \e[0;34m#{version[count]}\e[0m #{check}\n   #{description[count]}"
       count += 1
     end
-    range = (0..count).to_a
-    input_packages = STDIN.gets.chomp.to_i
-
-    install_pkg(input_packages) if range.include?(input_packages)
+    # range = (0..count).to_a
+    # input_packages = STDIN.gets.chomp.to_i
+    # install_pkg(input_packages) if range.include?(input_packages)
   end
 
   def help
