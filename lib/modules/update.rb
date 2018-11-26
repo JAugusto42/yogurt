cfaf5ef1525aeac0b59ba0fedf3769320b284908
José Augusto <joseaugusto.881@outlook.com>
2018-11-26 18:52:09 -0200

    modify update.rb

--- lib/modules/update.rb
+++ lib/modules/update.rb
@@ -11,7 +11,7 @@ module Update
       name_aur_pkg = "#{pkg_name}".split[0] # get name package
       pkg_local_version = "#{pkg_name}".split[1] # get local packages version
 
-      url = "https://aur.archlinux.org/rpc/?v=5&type=search&arg=#{name_aur_pkg}"
+      url = "https://aur.archlinux.org//rpc/?v=5&type=search&search_by=name&arg=#{name_aur_pkg}"
       buffer = open(url).read
       obj = JSON.parse(buffer)
       packages_name = obj['results']
