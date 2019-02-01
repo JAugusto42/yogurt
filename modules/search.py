2ccaaa9885d5ab6b576e0a8e111dde934699b0f2
Jose Augusto <joseaugusto.881@outlook.com>
2019-02-01 07:12:59 -0200

    update search and main file

--- modules/search.py
+++ modules/search.py
@@ -4,6 +4,7 @@ import os
 
 
 def search(package):
+    global pkgs
     print(':: Search ' + '\x1b[6;30;42m' + package + '\x1b[0m' + ' on aur...')
     url = "https://aur.archlinux.org/rpc/?v=5&type=search&arg=" + package
     url_json = requests.get(url).json()
@@ -15,7 +16,7 @@ def search(package):
     else:
         print(":: Found " + str(number_of_packages) + " packages")
 
-    #local_packages = os.system('pacman -Qm')
+    local_packages = os.listdir("/var/lib/pacman/local/")
 
     count = 0
     option = 1
@@ -23,7 +24,7 @@ def search(package):
         name = packages[count]['Name']
         version = packages[count]['Version']
         description = packages[count]['Description']
-        print(name, version)
+        name_and_version = name + '-' + version
         print(str(option) + '\033[94m' + ' aur/' + name + '\033[0m' + ' ' + version)
         print('   ' + description)
         option += 1
