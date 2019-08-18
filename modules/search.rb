c8896d9c8707001c38f169667c0495938d9ad4f1
Jose Augusto <joseaugusto.881@outlook.com>
2019-08-18 15:33:23 -0300

    return a nice msg when have no internet

--- modules/search.rb
+++ modules/search.rb
@@ -31,28 +31,33 @@ module Search
     end
 
     puts ":: Seaching #{pkg} on aur..."
-    url = "https://aur.archlinux.org/rpc/?v=5&type=search&arg=#{pkg}"
-    buffer = open(url).read
-    obj = JSON.parse(buffer)
-    packages = obj['resultcount']
-    packages_name = obj['results']
+    begin
+      url = "https://aur.archlinux.org/rpc/?v=5&type=search&arg=#{pkg}"
+      buffer = open(url).read
+      obj = JSON.parse(buffer)
+      packages = obj['resultcount']
+      packages_name = obj['results']
 
-    names = packages_name.map { |result| result['Name'] }
-    version = packages_name.map { |result| result['Version'] }
-    description = packages_name.map { |result| result['Description'] }
+      names = packages_name.map { |result| result['Name'] }
+      version = packages_name.map { |result| result['Version'] }
+      description = packages_name.map { |result| result['Description'] }
 
-    puts ":: Found #{packages} packages"
-    count = 0
-    exit unless packages > count
-    while count < packages
-      name_and_version = "#{names[count]}-#{version[count]}"
-      check = printf("\e[1;34mInstalled\e[0m ") if packages_local.include?(name_and_version)
-      puts ":: \e[1;32m#{count}\e[0m aur/#{names[count]} \e[0;32m#{version[count]}\e[0m #{check}"
-      puts "  #{description[count]}"
-      count += 1
-    end
+      puts ":: Found #{packages} packages"
+      count = 0
+      exit unless packages > count
+      while count < packages
+        name_and_version = "#{names[count]}-#{version[count]}"
+        check = printf("\e[1;34mInstalled\e[0m ") if packages_local.include?(name_and_version)
+        puts ":: \e[1;32m#{count}\e[0m aur/#{names[count]} \e[0;32m#{version[count]}\e[0m #{check}"
+        puts "  #{description[count]}"
+        count += 1
+      end
     # range = (0..count).to_a
     # input_packages = STDIN.gets.chomp.to_i
     # install_pkg(input_packages) if range.include?(input_packages)
+    rescue SocketError
+      puts "\n:: Check your internet connection"
+      exit
+    end
   end
 end 
