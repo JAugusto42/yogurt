741df2e2c73564cc1500ba2d23240075c88bf373
Jose Augusto <joseaugusto.881@outlook.com>
2019-09-24 21:22:42 -0300

    update install

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 
 function main() {
   echo 'Installing yogurt aur helper!'
-  #TODO verify if already exists and removes...
+  # TODO verify if already exists and removes...
+
   sudo mkdir -p -v /opt/yogurt/
   sudo git clone https://github.com/JAugusto42/yogurt.git /opt/yogurt
   cd /opt/yogurt
